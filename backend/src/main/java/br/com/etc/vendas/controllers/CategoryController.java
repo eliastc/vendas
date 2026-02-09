a62cc6274f3a5072c432bc5e7577f3094c0dc535
Elias Calado <eliascalado@hotmail.com>
2026-02-09 19:23:51 -0300

    Category controller

---
+++ backend/src/main/java/br/com/etc/vendas/controllers/CategoryController.java
@@ -0,0 +1,24 @@
+package br.com.etc.vendas.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.etc.vendas.entities.Category;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoryController {
+
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+		List<Category> list = new ArrayList<>();
+		list.add(new Category(1L, "Livros"));
+		list.add(new Category(2L, "Mesas"));
+		return ResponseEntity.ok().body(list);
+	}
+}
--- backend/src/main/java/br/com/etc/vendas/entities/category.java
+++ backend/src/main/java/br/com/etc/vendas/entities/category.java
@@ -1,5 +1,50 @@
 package br.com.etc.vendas.entities;
 
-public class category {
+import java.util.Objects;
 
+public class Category {
+
+	private Long id;
+	private String nome;
+	
+	public Category() {		
+	}
+	
+	public Category(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}  
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
 }
