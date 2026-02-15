4b6baa30c627249835795039e8364995efa800f5
Elias Calado <eliascalado@hotmail.com>
2026-02-15 12:20:06 -0300

    Inserindo nova categoria

--- backend/src/main/java/br/com/etc/vendas/dto/CategoryDTO.java
+++ backend/src/main/java/br/com/etc/vendas/dto/CategoryDTO.java
@@ -7,6 +7,9 @@ public class CategoryDTO {
 	private Long id;
 	private String nome;
 	
+	public CategoryDTO() {
+    }
+	
 	public CategoryDTO(Long id, String nome) {
 		this.id = id;
 		this.nome = nome;
